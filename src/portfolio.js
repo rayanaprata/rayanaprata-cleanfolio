7b6ab5a203b4d94e228c5314a9133041718e12c5
rayanaprata <rayanaprata@uol.com.br>
2021-09-11 11:05:37 -0300

    initial commit

---
+++ src/portfolio.js
@@ -0,0 +1,63 @@
+const header = {
+  homepage: 'https://rayanaprata-cleanfolio.github.io/cleanfolio',
+  title: 'RP.',
+}
+
+const about = {
+  name: 'Rayana Prata',
+  role: 'Front End Developer Student',
+  description:
+    'I need to be always looking for personal and professional growth. I believe time is a precious but limited resource. I try to use it in the smartest and most efficient way possible.',
+  resume: 'https://drive.google.com/file/d/1xg4pPVtwCR-7LkAkaJItSE9EnAfzHF6O/view?usp=sharing',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/rayanaprata/en',
+    github: 'https://github.com/rayanaprata',
+  },
+}
+
+const projects = [
+  {
+    name: 'Dev Finances',
+    description: 
+      'Project for personal finance control developed during the first edition of Marathon Discover offered by Rocketseat.',
+    stack: ['HTML', 'CSS', 'Javascript'],
+    sourceCode: 'https://github.com/rayanaprata/dev-finances',
+    livePreview: 'https://rayanaprata.github.io/dev-finances/',
+  },
+  {
+    name: 'To do list',
+    description:
+      'Task list project, stores the data in LocalStorage, developed using only HTML, CSS and Javascript and made together with Dev Ed\'s youtube channel.',
+    stack: ['HTML', 'CSS', 'Javascript'],
+    sourceCode: 'https://github.com/rayanaprata/todo-list',
+    livePreview: 'https://rayanaprata.github.io/todo-list/',
+  },
+  {
+    name: 'Dashboard Star Wars',
+    description:
+      'Dashboard project using Star Wars API to learn and practice requests with Axios.',
+    stack: ['HTML', 'CSS', 'Javascript', 'Axios', 'JQuery', 'SWAPI - The Star Wars API', 'Moment.js', 'Google Charts'],
+    sourceCode: 'https://github.com/rayanaprata/dashboard-star-wars',
+    livePreview: 'https://rayanaprata.github.io/dashboard-star-wars/',
+  },
+]
+
+const skills = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Styled Components',
+  'SASS',
+  'REST APIs',
+  'Git/GitHub',
+  'UI/UX Design',
+  'Figma/Adobe XD/Photoshop',
+]
+
+const contact = {
+  email: 'rayanaprata@uol.com.br',
+}
+
+export { header, about, projects, skills, contact }
